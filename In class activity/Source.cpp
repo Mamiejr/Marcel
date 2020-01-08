36d0fc16e96019299ce7bc817c7c91db3158ffff
donteamie@gmail.com <donteamie@gmail.com>
2020-01-08 14:53:44 -0700

    first commit

---
+++ In class activity/Source.cpp
@@ -0,0 +1,11 @@
+//Marcel Amie Jr.
+#include <iostream>
+#include <string>
+using namespace std;
+int main() {
+	float kilogram, pound;
+		cout << "Enter pound";
+		cin >> pound;
+		kilogram = pound * .454;
+		cout << pound << " pound = " << kilogram << " kg";
+}
